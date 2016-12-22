9de5249b3186fde23583cbba729feef3c2c40937
Nim <nim@Nims-MBP.local>
2016-12-21 20:09:27 -0800

    Set up linter and 'Hello World' with React; add README instructions to handle unmet peer dependencies

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # trip-raptor
 plan your voyage raptor-style
 
+[![npm](https://img.shields.io/npm/v/npm.svg)]()
 [![npm](https://img.shields.io/npm/l/express.svg)]()
 
+
 Execute ```npm run dev``` in the terminal. Dev server is found on port 8080.
+See [this comment](https://github.com/eslint/eslint/issues/7338#issuecomment-252785808)
+on how to handle unmet peer dependencies when running `npm install`.
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 module.exports = {
-  entry: "./src/client/scripts/app.js",
+  entry: './src/client/scripts/app.js',
   output: {
-    filename: "./dist/build/bundle.js"
-  }
-}
+    filename: './dist/build/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.es6'],
+  },
+};
