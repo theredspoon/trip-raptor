48ae5c732feb05c58be6826163f63718e9d1038c
Eric Lau <elau002@gmail.com>
2016-12-26 23:53:22 -0800

    Created Basic Routing for App

--- src/client/scripts/app.js
+++ src/client/scripts/app.js
@@ -1,2 +1,13 @@
-import { Home } from './components/home/home';
-console.log('app is loaded');
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+import Home from './components/home/home';
+import Planner from './components/planner/planner';
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Home} />
+    <Route path="/planner" component={Planner} />
+  </Router>
+), document.getElementById('container'));
---
+++ src/client/scripts/components/planner/planner.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Planner extends Component {
+  render() {
+    return (<h1>Planner</h1>);
+  }
+}
+
+export default Planner;
