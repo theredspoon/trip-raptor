2ce205e5b9a3f5525106eed58bc441ea2eaecf04
Eric Lau <elau002@gmail.com>
2017-01-02 20:10:27 -0800

    Future query search feature

--- src/client/scripts/components/place_input.jsx
+++ src/client/scripts/components/place_input.jsx
@@ -14,8 +14,22 @@ class Place extends Component {
           onChange={this.handleChange}
           style={{ width: '50%' }}
           onPlaceSelected={(place) => {
+            // need to set city name, viewport, and location ID
             console.log(place);
-            const = 
+            /* the following is a request for points of interests within the vacinity of user's input
+            const service = new google.maps.places.PlacesService(document.createElement('container'));
+            const search = {
+              bounds: place.geometry.viewport,
+             // query type/name will be fetched from the store and results will be different depending on such
+              types: ['restaraunt'],
+            };
+            service.nearbySearch(search, (res) => {
+              console.log(res);
+              res.sort((a,b) => {
+                return b.rating - a.rating;
+              });
+              // filter by rating and return the first 5 results
+            });*/
           }}
         />
       </div>
