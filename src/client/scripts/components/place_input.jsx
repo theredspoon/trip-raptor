51594b98f79317b7fd6953e6cc32985b4dc62952
Eric Lau <elau002@gmail.com>
2017-01-02 19:57:47 -0800

    Updated place_input container

--- src/client/scripts/components/place_input.jsx
+++ src/client/scripts/components/place_input.jsx
@@ -1,1 +1,26 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Autocomplete from 'react-google-autocomplete';
 import '../../styles/place_input.scss';
+
+class Place extends Component {
+  render() {
+    return (
+      <div className="jumbotron" styleName="orange">
+        <h1>Tell me:</h1>
+        <h2 styleName="red"> Where are you going?</h2>
+        <Autocomplete
+          placeholder=""
+          onChange={this.handleChange}
+          style={{ width: '50%' }}
+          onPlaceSelected={(place) => {
+            console.log(place);
+            const = 
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default Place;
