ca9f3f7cbfc2811e78d426317b7e31e125be142c
Nim <nim@Nims-MBP.local>
2017-01-02 19:06:56 -0800

    Add redux combineReducers boilerplate

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducer from './reducers/combine_reducers';
 
 import Home from './components/home/home';
 import Planner from './components/planner/planner';
+
+const store = createStore(reducer);
 
 render((
   <Provider store={store}>
