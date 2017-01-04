c833e1ed0be6272472a1d9c79ef520ff8587026a
Eric Lau <elau002@gmail.com>
2017-01-03 19:32:28 -0800

    Add todo to location reducer

--- src/client/scripts/reducers/location_reducer.js
+++ src/client/scripts/reducers/location_reducer.js
@@ -8,6 +8,7 @@ const currentLocation = (state = initialState, action) => {
   switch (action.type) {
     case 'UPDATE_CURRENT_LOCATION':
       return Object.assign({}, state, {
+        // todo updated/fix to work with data flow
         info: action.info,
       });
     default:
