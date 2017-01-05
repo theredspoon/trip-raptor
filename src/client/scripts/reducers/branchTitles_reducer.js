d823f6567cf9969b5da9f36eb1d56415378c144c
Nim <nim@Nims-MBP.local>
2017-01-04 18:59:33 -0800

    Complete branchTitles reducer v1

--- src/client/scripts/reducers/branchTitles_reducer.js
+++ src/client/scripts/reducers/branchTitles_reducer.js
@@ -1,13 +1,14 @@
-const intialState = ['hotel', 'point of interest', 'museum', 'night club', 'restarant'];
+const intialState = ['hotels', 'points of interest', 'museums', 'nightlife', 'restaurants'];
 
 const branchTitles = (state = intialState, action) => {
   switch (action.type) {
-    // case 'UPDATE_BRANCH_TITLES':
-    // return state
+    case 'UPDATE_BRANCH_TITLES':
+      return Object.assign({}, state, {
+        branchTitles: action.payload,
+      });
     default:
       return state;
   }
 };
 
 export default branchTitles;
-
