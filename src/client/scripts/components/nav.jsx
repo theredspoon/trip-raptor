45c3d314561c4b1c90a4e2097f7bef33ee862fdb
Eric Lau <elau002@gmail.com>
2017-01-11 18:44:24 -0800

    NavBar container created to replace similar nav el. through app

--- src/client/scripts/components/nav.jsx
+++ src/client/scripts/components/nav.jsx
@@ -1,2 +1,61 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+
 import '../../styles/nav.scss';
 
+const mapStateToProps = state =>
+   ({
+     currentLocation: state.currentLocation,
+     currentRoot: state.currentRoot,
+     routing: state.routing,
+   });
+
+const mapDispatchToProps = dispatch => ({
+  goHome: () => {
+    dispatch(push('/'));
+  },
+});
+
+class NavBar extends Component {
+  render() {
+    const localRoot = this.props.currentRoot.currentRoot;
+    const currentCity = this.props.currentLocation.city;
+    const pathName = this.props.routing.locationBeforeTransitions.pathname;
+
+    let navBar = null;
+
+    if (pathName === '/city') {
+      navBar = (
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <a className="navbar-brand" onClick={() => this.props.goHome()}>TRIP_RAPTOR
+            <span> | Planning your trip to {currentCity} city</span>
+            </a>
+          </div>
+        </nav>);
+    } else if (pathName.includes(`/city/${localRoot}`)) {
+      navBar = (
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <a className="navbar-brand" onClick={() => this.props.goHome()}>TRIP_RAPTOR
+            <span> | Choosing {localRoot} you like</span>
+            </a>
+          </div>
+        </nav>);
+    } else {
+      navBar = (
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <a className="navbar-brand" onClick={() => this.props.goHome()}>TRIP_RAPTOR</a>
+          </div>
+        </nav>);
+    }
+    return (
+      <div>
+        {navBar}
+      </div>
+    );
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
